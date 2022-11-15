c254a5cbacc54d6c09a179004111f2e987e8ab29
Kyle Jorve <kyle.jorve@gmail.com>
2022-11-15 11:58:19 -0800

    exercise 4 complete

--- components/comic/Detail.tsx
+++ components/comic/Detail.tsx
@@ -4,17 +4,25 @@ import styles from "../../styles/Comics.module.css";
 export default function Detail(props: ComicDeets) {
     return (
         <ul className={styles["comic-item__detail-list"]}>
-            <li>
-                <strong>Issue:</strong> {props.issueNumber}
-            </li>
-
-            <li>
-                <strong>Published:</strong> {props.publishDate}
-            </li>
-
-            <li>
-                <strong>Creators:</strong> {props.creators.map((c) => c.name).join(", ")}
-            </li>
+            {!!props.issueNumber && (
+                <li data-testid="issue-li">
+                    <strong>Issue:</strong> {props.issueNumber}
+                </li>
+            )}
+            {!!props.publishDate && (
+                <li data-testid="publish-date-li">
+                    <strong>Published:</strong> {props.publishDate}
+                </li>
+            )}
+            {!!props.creators?.length && (
+                <li data-testid="creators-li">
+                    <strong>Creators:</strong>{" "}
+                    {props.creators
+                        .map((c) => c.name)
+                        .filter((c) => c)
+                        .join(", ")}
+                </li>
+            )}
         </ul>
     );
 }
---
+++ tests/index.test.tsx
@@ -0,0 +1,41 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Detail from "../components/comic/Detail";
+
+describe("Detail component", () => {
+    it("renders all comic details correctly", () => {
+        const testData = {
+            issueNumber: 4,
+            publishDate: "Neverbruary 39, 3013",
+            creators: [
+                {
+                    resourceURI: "blah blah blah",
+                    name: "Test Testerson",
+                    role: "a guy",
+                },
+                {
+                    resourceURI: "totally a real URL",
+                    name: "Dumb Idiot",
+                    role: "the dumbest idiot",
+                },
+                {
+                    resourceURI: "definitely click on this",
+                    name: "Buttercup McButtface",
+                    role: "a person",
+                },
+            ],
+        };
+
+        render(
+            <Detail
+                issueNumber={testData.issueNumber}
+                publishDate={testData.publishDate}
+                creators={testData.creators}
+            />,
+        );
+
+        expect(screen.getByTestId("issue-li")).toBeInTheDocument();
+        expect(screen.getByTestId("publish-date-li")).toBeInTheDocument();
+        expect(screen.getByTestId("creators-li")).toBeInTheDocument();
+    });
+});
